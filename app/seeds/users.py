ec5c052623f13cbe50f77fd335cd6bcc91dbfbf6
Martyn <martynodlrr@gmail.com>
2023-09-07 18:28:12 -0500

    updated users seed file

--- app/seeds/users.py
+++ app/seeds/users.py
@@ -4,23 +4,23 @@ from sqlalchemy.sql import text
 
 def seed_users():
     demo = User(
-        first_name='Demo', last_name='Lition', username='Demo', email='demo@aa.io', password='password', pfp="https://www.google.com/url?sa=i&url=https%3A%2F%2Fwww.pinterest.com%2F29bribri29%2Fbasic-pfp%2F&psig=AOvVaw3PVvDCjglzzwbjRgr6Nx7B&ust=1694205350893000&source=images&cd=vfe&opi=89978449&ved=0CA8QjRxqFwoTCNieoPOsmYEDFQAAAAAdAAAAABAE")
+        first_name='Demo', last_name='Lition', username='Demo', email='demo@aa.io', password='password', pfp="https://i.pinimg.com/originals/09/f0/84/09f084cd8a8093ea5738a3ab75c210df.png")
     marnie = User(
-        first_name='Marnie', last_name='Carnie', username='marnie', email='marnie@aa.io', password='password', pfp="https://www.google.com/url?sa=i&url=https%3A%2F%2Fwww.pinterest.com%2F29bribri29%2Fbasic-pfp%2F&psig=AOvVaw3PVvDCjglzzwbjRgr6Nx7B&ust=1694205350893000&source=images&cd=vfe&opi=89978449&ved=0CA8QjRxqFwoTCNieoPOsmYEDFQAAAAAdAAAAABAE")
+        first_name='Marnie', last_name='Carnie', username='marnie', email='marnie@aa.io', password='password', pfp="https://i.pinimg.com/originals/09/f0/84/09f084cd8a8093ea5738a3ab75c210df.png")
     bobbie = User(
-        first_name='Bobbie', last_name='Shmobbie', username='bobbie', email='bobbie@aa.io', password='password', pfp="https://www.google.com/url?sa=i&url=https%3A%2F%2Fwww.pinterest.com%2F29bribri29%2Fbasic-pfp%2F&psig=AOvVaw3PVvDCjglzzwbjRgr6Nx7B&ust=1694205350893000&source=images&cd=vfe&opi=89978449&ved=0CA8QjRxqFwoTCNieoPOsmYEDFQAAAAAdAAAAABAE")
+        first_name='Bobbie', last_name='Shmobbie', username='bobbie', email='bobbie@aa.io', password='password', pfp="https://i.pinimg.com/originals/09/f0/84/09f084cd8a8093ea5738a3ab75c210df.png")
     martyn = User(
-        first_name='Martyn', last_name="O'Connor DeLosRios-Roberts", username='martyn', email='martyn@aa.io', password='password', pfp="https://www.google.com/url?sa=i&url=https%3A%2F%2Fwww.pinterest.com%2F29bribri29%2Fbasic-pfp%2F&psig=AOvVaw3PVvDCjglzzwbjRgr6Nx7B&ust=1694205350893000&source=images&cd=vfe&opi=89978449&ved=0CA8QjRxqFwoTCNieoPOsmYEDFQAAAAAdAAAAABAE")
+        first_name='Martyn', last_name="O'Connor DeLosRios-Roberts", username='martyn', email='martyn@aa.io', password='password', pfp="https://i.pinimg.com/originals/09/f0/84/09f084cd8a8093ea5738a3ab75c210df.png")
     alex = User(
-        first_name='Alex', last_name='Lexie', username='alex', email='alex@aa.io', password='password', pfp="https://www.google.com/url?sa=i&url=https%3A%2F%2Fwww.pinterest.com%2F29bribri29%2Fbasic-pfp%2F&psig=AOvVaw3PVvDCjglzzwbjRgr6Nx7B&ust=1694205350893000&source=images&cd=vfe&opi=89978449&ved=0CA8QjRxqFwoTCNieoPOsmYEDFQAAAAAdAAAAABAE")
+        first_name='Alex', last_name='Lexie', username='alex', email='alex@aa.io', password='password', pfp="https://i.pinimg.com/originals/09/f0/84/09f084cd8a8093ea5738a3ab75c210df.png")
     taylor = User(
-        first_name='Taylor', last_name='Tailor', username='taylor', email='taylor@aa.io', password='password', pfp="https://www.google.com/url?sa=i&url=https%3A%2F%2Fwww.pinterest.com%2F29bribri29%2Fbasic-pfp%2F&psig=AOvVaw3PVvDCjglzzwbjRgr6Nx7B&ust=1694205350893000&source=images&cd=vfe&opi=89978449&ved=0CA8QjRxqFwoTCNieoPOsmYEDFQAAAAAdAAAAABAE")
+        first_name='Taylor', last_name='Tailor', username='taylor', email='taylor@aa.io', password='password', pfp="https://i.pinimg.com/originals/09/f0/84/09f084cd8a8093ea5738a3ab75c210df.png")
     jordan = User(
-        first_name='Jordan', last_name='Jordanie', username='jordan', email='jordan@aa.io', password='password', pfp="https://www.google.com/url?sa=i&url=https%3A%2F%2Fwww.pinterest.com%2F29bribri29%2Fbasic-pfp%2F&psig=AOvVaw3PVvDCjglzzwbjRgr6Nx7B&ust=1694205350893000&source=images&cd=vfe&opi=89978449&ved=0CA8QjRxqFwoTCNieoPOsmYEDFQAAAAAdAAAAABAE")
+        first_name='Jordan', last_name='Jordanie', username='jordan', email='jordan@aa.io', password='password', pfp="https://i.pinimg.com/originals/09/f0/84/09f084cd8a8093ea5738a3ab75c210df.png")
     morgan = User(
-        first_name='Morgan', last_name='Morganie', username='morgan', email='morgan@aa.io', password='password', pfp="https://www.google.com/url?sa=i&url=https%3A%2F%2Fwww.pinterest.com%2F29bribri29%2Fbasic-pfp%2F&psig=AOvVaw3PVvDCjglzzwbjRgr6Nx7B&ust=1694205350893000&source=images&cd=vfe&opi=89978449&ved=0CA8QjRxqFwoTCNieoPOsmYEDFQAAAAAdAAAAABAE")
+        first_name='Morgan', last_name='Morganie', username='morgan', email='morgan@aa.io', password='password', pfp="https://i.pinimg.com/originals/09/f0/84/09f084cd8a8093ea5738a3ab75c210df.png")
     casey = User(
-        first_name='Casey', last_name='Casie', username='casey', email='casey@aa.io', password='password', pfp="https://www.google.com/url?sa=i&url=https%3A%2F%2Fwww.pinterest.com%2F29bribri29%2Fbasic-pfp%2F&psig=AOvVaw3PVvDCjglzzwbjRgr6Nx7B&ust=1694205350893000&source=images&cd=vfe&opi=89978449&ved=0CA8QjRxqFwoTCNieoPOsmYEDFQAAAAAdAAAAABAE")
+        first_name='Casey', last_name='Casie', username='casey', email='casey@aa.io', password='password', pfp="https://i.pinimg.com/originals/09/f0/84/09f084cd8a8093ea5738a3ab75c210df.png")
 
     db.session.add(demo)
     db.session.add(marnie)
